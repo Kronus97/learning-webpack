7f85935be68261de1a28f76dadc0a26d30318323
Kronu97 <mihai.soml3a@gmail.com>
2021-06-17 22:24:55 +0300

    started working on day 2

---
+++ day2/src/image.js
@@ -0,0 +1,7 @@
+import Sunset from "./static/images/sunset.jpg";
+
+function addBackgroundImage() {
+    document.querySelector('body').style.background = `url(${Sunset}) center no-repeat`;
+}
+
+export default addBackgroundImage;
---
+++ day2/src/index.js
@@ -0,0 +1,7 @@
+import { Counter } from "./components/counter.js";
+import addBackgroundImage from "./image.js";
+
+const counter = new Counter();
+counter.render();
+
+addBackgroundImage();
