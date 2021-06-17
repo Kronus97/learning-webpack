26e7e1c45fdda5aa55669f90d8ed04cd0a7fbc9a
Kronu97 <mihai.soml3a@gmail.com>
2021-06-17 22:41:35 +0300

    added plugin for moving css styling

--- day2/webpack.config.js
+++ day2/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -22,7 +23,7 @@ module.exports = {
             {
                 test: /\.(css)$/,
                 use: [
-                    'style-loader', 'css-loader'
+                    MiniCssExtractPlugin.loader, 'css-loader'
                 ]
             },
             {
@@ -39,6 +40,9 @@ module.exports = {
         ]
     },
     plugins: [
-        new TerserPlugin()
+        new TerserPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'styles.css'
+        })
     ]
 }
