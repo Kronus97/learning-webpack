45f2f94e21e0af06c145dd99b2e1443bc08c9ca0
Kronu97 <mihai.soml3a@gmail.com>
2021-06-18 22:27:24 +0300

    added eslint configs

---
+++ day2/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": ["error", 4],
+        "semi": ["error", "always"],
+        "no-console": ["error", { allow: ["warn", "error"]}]
+    }
+}
