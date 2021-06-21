a4cd5a46d97193f105bfe7c3f6d628ef2f35c582
Kronu97 <mihai.soml3a@gmail.com>
2021-06-21 18:12:05 +0300

    added prettier configs and updated eslint configs

--- day3/.eslintrc.js
+++ day3/.eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-    "extends": "eslint:recommended",
-    "rules": {
-        "indent": ["error", 4],
-        "semi": ["error", "always"],
-        "no-console": ["error", { allow: ["warn", "error"]}]
-    }
+    extends: ['eslint:recommended', 'prettier'],
+    rules: {
+        indent: ['error', 4],
+        semi: ['error', 'always'],
+        'no-console': ['error', { allow: ['warn', 'error'] }],
+    },
 }
